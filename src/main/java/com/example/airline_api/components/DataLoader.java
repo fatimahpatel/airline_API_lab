313b33a18cba006db780439176cdf602447e3965
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-08 22:39:22 +0000

    Created new Flight called edinburgh

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,11 +1,14 @@
 package com.example.airline_api.components;
 
+import com.example.airline_api.models.Flight;
 import com.example.airline_api.repository.FlightRepository;
 import com.example.airline_api.repository.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.GregorianCalendar;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -17,5 +20,15 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        //BRITISH AIRWAYS
+        Flight britishAirways = new Flight("London", 20, "8/12/2022", "08:00");
+        flightRepository.save(britishAirways);
+
+
+
+
+
+
+
     }
 }
