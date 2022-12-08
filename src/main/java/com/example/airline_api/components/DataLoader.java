d37534b4a11194c78f13b32bf51618ec2f68f0cc
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-08 22:07:30 +0000

    Autowired FlightRepository and PassengerRepository

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,13 +1,21 @@
 package com.example.airline_api.components;
 
+import com.example.airline_api.repository.FlightRepository;
+import com.example.airline_api.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements ApplicationRunner {
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
     }
 }
