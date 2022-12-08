84293b2ec140233d80a81d8a3684e38fdde7383a
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-08 21:56:13 +0000

    Edited getPassengerById()

--- src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/passengers")
@@ -24,14 +25,12 @@ public class PassengerController {
 
     // Display specific passenger details
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
-    }
+    public ResponseEntity<Optional<Passenger>> getPassengerById(@PathVariable Long id){
+        return new ResponseEntity<>(passengerRepository.findById(id), HttpStatus.OK);
 
     // Add a new passenger
     @PostMapping
     public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
-    }
+        return n
 
 }
