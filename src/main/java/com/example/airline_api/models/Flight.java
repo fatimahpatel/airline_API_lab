4b1da7df3bd709e6b50b3f008f6e7485440ded51
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-08 20:09:33 +0000

    Removed ] from line 14

--- src/main/java/com/example/airline_api/models/Flight.java
+++ src/main/java/com/example/airline_api/models/Flight.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Flight {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY])
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private long id;
     @Column
